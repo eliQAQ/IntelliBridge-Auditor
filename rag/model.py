b257ec8036db3f6a4bbe35a0aeaca9279b98b689
wangyu150 <71958060+wangyu150@users.noreply.github.com>
2025-07-14 12:41:15 +0800

    Update model.py

--- rag/model.py
+++ rag/model.py
@@ -7,7 +7,9 @@ import requests
 total_completion_tokens = 0
 total_prompt_tokens = 0
 total_cost = 0
-
+this_cost = 0
+this_completion_tokens = 0
+this_prompt_tokens = 0
 # openai
 OPENAI_API = ""
 client = OpenAI(api_key=OPENAI_API)
@@ -15,18 +17,33 @@ client = OpenAI(api_key=OPENAI_API)
 # 硅基流动
 url = "https://api.siliconflow.cn/v1/chat/completions"
 gjld_headers = {
-    "Authorization": "",
+    "Authorization": "",#此处输入硅基流动key，Bearer xxx 
     "Content-Type": "application/json"
 }
-def gpt(prompt, model='deepseek-ai/DeepSeek-V3', temperature=0.7, max_tokens=4096, stop=None, platform = "gjld") -> tuple[
-    list[Union[Optional[str], Any]], int, int]:
+
+
+def clean_this_cost():
+    global this_cost, this_prompt_tokens, this_completion_tokens
+    this_cost = 0
+    this_completion_tokens = 0
+    this_prompt_tokens = 0
+
+def get_this_cost():
+    return this_cost, this_prompt_tokens, this_completion_tokens
+def gpt(prompt, model='deepseek-ai/DeepSeek-V3', temperature=0.7, max_tokens=4096, stop=None, platform = "gjld") -> \
+tuple[list[str | None | Any], int, int, Any]:
     messages = [{"role": "user", "content": prompt}]
     return chatgpt(messages, model=model, temperature=temperature, max_tokens=max_tokens, stop=stop,platform=platform)
 
+def gpt_with_message(prompt, messages:list, model='deepseek-ai/DeepSeek-V3', temperature=0.7, max_tokens=4096, stop=None, platform = "gjld") -> \
+tuple[list[str | None | Any], int, int, Any]:
+    messages.append({"role": "user", "content": prompt})
+    return chatgpt(messages, model=model, temperature=temperature, max_tokens=max_tokens, stop=stop,platform=platform)
 
-def chatgpt(messages, model='deepseek-ai/DeepSeek-V3', temperature=0.7, max_tokens=4096, stop=None, platform="gjld") -> tuple[
-    list[Union[Optional[str], Any]], int, int]:
-    global total_completion_tokens, total_prompt_tokens, total_cost
+
+def chatgpt(messages, model='deepseek-ai/DeepSeek-V3', temperature=0.7, max_tokens=4096, stop=None, platform="gjld") -> \
+tuple[list[str | None | Any], int, int, Any]:
+    global total_completion_tokens, total_prompt_tokens, total_cost, this_cost, this_prompt_tokens, this_completion_tokens
     native_completion_tokens = 0
     native_prompt_tokens = 0
     outputs = []
@@ -48,15 +65,29 @@ def chatgpt(messages, model='deepseek-ai/DeepSeek-V3', temperature=0.7, max_toke
             "response_format": {"type": "json_object"},
             "tools": []
         }
-        res = requests.request("POST", url, json=payload, headers=gjld_headers)
-        res = res.json()
-        if "code" in res and res["code"] > 200:
-            raise Exception(res["message"])
-        outputs.extend([choice["message"]["content"] for choice in res["choices"]])
+        while True:
+            try:
+                res = requests.request("POST", url, json=payload, headers=gjld_headers, timeout=300)
+                res = res.json()
+                if "code" in res and res["code"] > 200:
+                    raise Exception(res["message"])
+                if "choices" not in res:
+                    print(res)
+                    # 暂停1秒
+                    time.sleep(1)
+                    continue
+                outputs.extend([choice["message"]["content"] for choice in res["choices"]])
+                break
+            except requests.exceptions.RequestException as e:
+                print("请求api超时，重新请求")
+
 
         # log completion tokens
         total_completion_tokens += res["usage"]["completion_tokens"]
         total_prompt_tokens += res["usage"]["prompt_tokens"]
+
+        this_completion_tokens += res["usage"]["completion_tokens"]
+        this_prompt_tokens += res["usage"]["prompt_tokens"]
 
         native_completion_tokens += res["usage"]["completion_tokens"]
         native_prompt_tokens += res["usage"]["prompt_tokens"]
@@ -68,13 +99,17 @@ def chatgpt(messages, model='deepseek-ai/DeepSeek-V3', temperature=0.7, max_toke
         total_completion_tokens += res.usage.completion_tokens
         total_prompt_tokens += res.usage.prompt_tokens
 
+        this_completion_tokens += res.usage.completion_tokens
+        this_prompt_tokens += res.usage.prompt_tokens
+
         native_completion_tokens += res.usage.completion_tokens
         native_prompt_tokens += res.usage.prompt_tokens
     else:
         raise Exception("请选择支持的平台...")
-    total_cost += calculate_gpt_usage(native_completion_tokens, native_prompt_tokens, platform=platform, model=model)
-
-    return outputs, native_completion_tokens, native_prompt_tokens
+    cost = calculate_gpt_usage(native_completion_tokens, native_prompt_tokens, platform=platform, model=model)
+    total_cost += cost
+    this_cost += cost
+    return outputs, native_completion_tokens, native_prompt_tokens, messages
 
 
 def calculate_gpt_usage(completion_tokens, prompt_tokens, platform='gjld',model='deepseek-ai/DeepSeek-V3'):
