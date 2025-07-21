cfdce5e8e96e73545bf535a994a1d700e47d4ab4
wangyu150 <71958060+wangyu150@users.noreply.github.com>
2025-07-21 17:30:51 +0800

    Update model.py

--- rag/model.py
+++ rag/model.py
@@ -12,10 +12,14 @@ this_completion_tokens = 0
 this_prompt_tokens = 0
 # openai
 OPENAI_API = ""
-client = OpenAI(api_key=OPENAI_API)
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key=OPENAI_API,
+)
 
 # 硅基流动
 url = "https://api.siliconflow.cn/v1/chat/completions"
+# url = "https://api.siliconflow.cn/v1/messages"
 gjld_headers = {
     "Authorization": "",
     "Content-Type": "application/json"
@@ -30,7 +34,7 @@ def clean_this_cost():
 
 def get_this_cost():
     return this_cost, this_prompt_tokens, this_completion_tokens
-def gpt(prompt, model='deepseek-ai/DeepSeek-V3', temperature=0.7, max_tokens=4096, stop=None, platform = "gjld") -> \
+def gpt(prompt, model='deepseek-ai/DeepSeek-V3', temperature=0.7, max_tokens=16384, stop=None, platform = "gjld") -> \
 tuple[list[str | None | Any], int, int, Any]:
     messages = [{"role": "user", "content": prompt}]
     return chatgpt(messages, model=model, temperature=temperature, max_tokens=max_tokens, stop=stop,platform=platform)
@@ -41,7 +45,7 @@ tuple[list[str | None | Any], int, int, Any]:
     return chatgpt(messages, model=model, temperature=temperature, max_tokens=max_tokens, stop=stop,platform=platform)
 
 
-def chatgpt(messages, model='deepseek-ai/DeepSeek-V3', temperature=0.7, max_tokens=4096, stop=None, platform="gjld") -> \
+def chatgpt(messages, model='deepseek-ai/DeepSeek-V3', temperature=0.7, max_tokens=8192, stop=None, platform="gjld") -> \
 tuple[list[str | None | Any], int, int, Any]:
     global total_completion_tokens, total_prompt_tokens, total_cost, this_cost, this_prompt_tokens, this_completion_tokens
     native_completion_tokens = 0
@@ -77,6 +81,11 @@ tuple[list[str | None | Any], int, int, Any]:
                     # 暂停1秒
                     time.sleep(1)
                     continue
+                if not res["choices"][0]["message"]["content"]:
+                    print(res)
+                    # 暂停1秒
+                    time.sleep(1)
+                    continue
                 outputs.extend([choice["message"]["content"] for choice in res["choices"]])
                 break
             except requests.exceptions.RequestException as e:
@@ -93,9 +102,13 @@ tuple[list[str | None | Any], int, int, Any]:
         native_completion_tokens += res["usage"]["completion_tokens"]
         native_prompt_tokens += res["usage"]["prompt_tokens"]
     elif platform == "openai":
-        res = client.chat.completions.create(model=model, messages=messages, temperature=temperature, max_tokens=max_tokens,
-                                   n=1, stop=stop, response_format={"type": "json_object"})
+        # res = client.chat.completions.create(model=model, messages=messages, temperature=temperature, max_tokens=max_tokens,
+        #                            n=1, stop=stop, response_format={"type": "json_object"})
+        res = client.chat.completions.create(model=model, messages=messages, max_tokens=max_tokens, n=1, stop=stop, response_format={"type": "json_object"})
         outputs.extend([choice.message.content for choice in res.choices])
+        if model == "moonshotai/kimi-k2" or model == "deepseek/deepseek-r1-0528":
+            for i in range(len(outputs)):
+                outputs[i] = outputs[i].lstrip("```").rstrip("```").lstrip("json")
         # log completion tokens
         total_completion_tokens += res.usage.completion_tokens
         total_prompt_tokens += res.usage.prompt_tokens
@@ -115,12 +128,16 @@ tuple[list[str | None | Any], int, int, Any]:
 
 def calculate_gpt_usage(completion_tokens, prompt_tokens, platform='gjld',model='deepseek-ai/DeepSeek-V3'):
     cost = 0
-    if platform == 'openai':
-        if model == "gpt-4":
-            cost = completion_tokens / 1000 * 0.06 + prompt_tokens / 1000 * 0.03
-        elif model == "gpt-3.5-turbo":
-            cost = completion_tokens / 1000 * 0.002 + prompt_tokens / 1000 * 0.0015
-    elif platform == 'gjld':
+    if platform == 'openai':# 美元
+        if model == "openai/gpt-4o":
+            cost = completion_tokens / 1000 * 0.0025 + prompt_tokens / 1000 * 0.01
+        elif model == "google/gemini-2.5-pro":
+            cost = completion_tokens / 1000 * 0.00125 + prompt_tokens / 1000 * 0.01
+        elif model == "moonshotai/kimi-k2":
+            cost = completion_tokens / 1000 * 0.00014 + prompt_tokens / 1000 * 0.00249
+        elif model == "deepseek/deepseek-r1-0528":
+            cost = completion_tokens / 1000 * 0.000272 + prompt_tokens / 1000 * 0.000272
+    elif platform == 'gjld':# 人民币
         if model.startswith("Pro/"):
             model = model.replace("Pro/", "")
         if model == 'deepseek-ai/DeepSeek-V3':
